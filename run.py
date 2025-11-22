428d1e1ae28ec74011ee9bc8d1b93c212ff13588
guddupoll <rajjatrk009@gmail.com>
2025-11-22 10:46:31 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os,sys,platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    os.system('chmod +x tld')
-    os.system('./tld')
+    os.system('chmod +x guddu')
+    os.system('./guddu')
 elif bit == '32bit':
-    os.system('chmod +x tld32')
-    os.system('./tld32')
+    os.system('chmod +x guddu32')
+    os.system('./guddu32')
 else:
     print('device not supported')
